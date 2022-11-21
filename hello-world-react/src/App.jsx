9db14131063d0d8a2bf051da80f9e426a8571e21
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-21 10:04:41 -0300

    Usando components no React

--- hello-world-react/src/App.jsx
+++ hello-world-react/src/App.jsx
@@ -1,8 +1,8 @@
-import HelloWorld from './components/HelloWorld'
+import About from './components/About'
 
 function App() {
   return (
-    <HelloWorld />
+    <About />
   )
 }
 
---
+++ hello-world-react/src/components/About/index.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+
+class About extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Walber Vaz (w2k)</h1>
+        <p>Estudando de Programação web FullStack</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Dedicado</li>
+          <li>Esforçado</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default About
