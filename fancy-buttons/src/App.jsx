22f4f5e800dccd0aa10132e2ead0fddd6b6deff1
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-29 13:23:17 -0300

    Usando State

--- fancy-buttons/src/App.jsx
+++ fancy-buttons/src/App.jsx
@@ -8,28 +8,86 @@ class App extends Component {
     //   numberClick: 0,
     // };
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClickBtn1 = this.handleClickBtn1.bind(this);
+    this.handleClickBtn2 = this.handleClickBtn2.bind(this);
+    this.handleClickBtn3 = this.handleClickBtn3.bind(this);
   }
 
   // Public class fields
   state = {
-    numberClick: 0,
+    numberClickBtn1: 0,
+    numberClickBtn2: 0,
+    numberClickBtn3: 0,
   };
 
   // Pode fazer uma Arrow Function no lugar do bind
 
-  handleClick() {
-    this.setState((state) => ({
-      numberClick: state.numberClick + 1,
-    }));
+  handleClickBtn1(param) {
+    const { numberClickBtn1 } = this.state;
+    this.setState(
+      (state) => ({
+        numberClickBtn1: state.numberClickBtn1 + param,
+      }),
+      () => {
+        console.log(`Btn1: ${numberClickBtn1}`);
+      },
+    );
+  }
+
+  handleClickBtn2(param) {
+    const { numberClickBtn2 } = this.state;
+    this.setState(
+      (state) => ({
+        numberClickBtn2: state.numberClickBtn2 + param,
+      }),
+      () => {
+        console.log(`Btn2: ${numberClickBtn2}`);
+      },
+    );
+  }
+
+  handleClickBtn3(param) {
+    const { numberClickBtn3 } = this.state;
+    this.setState(
+      (state) => ({
+        numberClickBtn3: state.numberClickBtn3 + param,
+      }),
+      () => {
+        console.log(`Btn3: ${numberClickBtn3}`);
+      },
+    );
+  }
+
+  getBtnColor(param) {
+    return param % 2 === 0 ? 'green' : 'white';
   }
 
   render() {
-    const { numberClick } = this.state;
+    const { numberClickBtn1, numberClickBtn2, numberClickBtn3 } = this.state;
     return (
-      <button type="button" onClick={this.handleClick}>
-        {numberClick}
-      </button>
+      <div>
+        <button
+          type="button"
+          onClick={() => this.handleClickBtn1(1)}
+          style={{ backgroundColor: this.getBtnColor(numberClickBtn1) }}
+        >
+          {numberClickBtn1}
+        </button>
+        <button
+          type="button"
+          onClick={() => this.handleClickBtn2(1)}
+          style={{ backgroundColor: this.getBtnColor(numberClickBtn2) }}
+        >
+          {numberClickBtn2}
+        </button>
+        <button
+          type="button"
+          onClick={() => this.handleClickBtn3(1)}
+          style={{ backgroundColor: this.getBtnColor(numberClickBtn3) }}
+        >
+          {numberClickBtn3}
+        </button>
+      </div>
     );
   }
 }
