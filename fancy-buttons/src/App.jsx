a5c80d6fe94df2ed1c51476fa48d5a672c9ae7d5
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-29 12:59:56 -0300

    Usando Public Class Field

--- fancy-buttons/src/App.jsx
+++ fancy-buttons/src/App.jsx
@@ -3,17 +3,32 @@ import { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
+
+    // this.state = {
+    //   numberClick: 0,
+    // };
+
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // Public class fields
+  state = {
+    numberClick: 0,
+  };
+
+  // Pode fazer uma Arrow Function no lugar do bind
+
   handleClick() {
-    console.log('this is:', this);
+    this.setState((state) => ({
+      numberClick: state.numberClick + 1,
+    }));
   }
 
   render() {
+    const { numberClick } = this.state;
     return (
       <button type="button" onClick={this.handleClick}>
-        click
+        {numberClick}
       </button>
     );
   }
