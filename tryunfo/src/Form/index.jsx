cfc25e8f658f9c6021d6ab680ba7e5c32f1c06f0
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-02 14:31:58 -0300

    Colocando as props do componente Form

--- tryunfo/src/Form/index.jsx
+++ tryunfo/src/Form/index.jsx
@@ -1,74 +1,134 @@
+import { bool, func, string } from 'prop-types';
 import React from 'react';
 import { ContainerForm, FormContainer, FormStyled } from './styles';
 
-function Form() {
+function Form(props) {
+  const {
+    cardName,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+    cardRare,
+    cardTrunfo,
+    // hasTrunfo,
+    isSaveButtonDisabled,
+    onInputChange,
+    onSaveButtonClick,
+  } = props;
+
   return (
     <ContainerForm>
       <FormContainer>
         <FormStyled>
           <h2>Adicione sua carta</h2>
-          <label htmlFor="">
+          <label htmlFor="cardName">
             Nome da carta:
             <input
               type="text"
               data-testid="name-input"
               placeholder="Digite nome da sua carta"
+              id="cardName"
+              name="cardName"
+              value={cardName}
+              onChange={onInputChange}
             />
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardDescription">
             Descrição da carta:
             <textarea
               data-testid="description-input"
               placeholder="Digite a descrição da sua carta"
               spellCheck="true"
               lang="pt-BR"
+              id="cardDescription"
+              name="cardDescription"
+              value={cardDescription}
+              onChange={onInputChange}
             />
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardAttr1">
             Atributo 1:
             <input
               type="number"
               data-testid="attr1-input"
               placeholder="Digite o valor do atributo 1"
+              id="cardAttr1"
+              name="cardAttr1"
+              value={cardAttr1}
+              onChange={onInputChange}
             />
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardAttr2">
             Atributo 2:
             <input
               type="number"
               data-testid="attr2-input"
               placeholder="Digite o valor do atributo 2"
+              id="cardAttr2"
+              name="cardAttr2"
+              value={cardAttr2}
+              onChange={onInputChange}
             />
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardAttr3">
             Atributo 3:
             <input
               type="number"
               data-testid="attr3-input"
               placeholder="Digite o valor do atributo 3"
+              id="cardAttr3"
+              name="cardAttr3"
+              value={cardAttr3}
+              onChange={onInputChange}
             />
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardImage">
             Imagem:
             <input
               type="text"
               data-testid="image-input"
               placeholder="Digite o link da imagem"
+              id="cardImage"
+              name="cardImage"
+              value={cardImage}
+              onChange={onInputChange}
             />
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardRare">
             Raridade:
-            <select data-testid="rare-input">
-              <option value="normal">Normal</option>
+            <select
+              data-testid="rare-input"
+              id="cardRare"
+              name="cardRare"
+              value={cardRare}
+              onChange={onInputChange}
+            >
+              <option defaultValue value="normal">
+                Normal
+              </option>
               <option value="raro">Raro</option>
               <option value="muito raro">Muito Raro</option>
             </select>
           </label>
-          <label htmlFor="">
+          <label htmlFor="cardTrunfo">
             Super Trunfo:
-            <input type="checkbox" data-testid="trunfo-input" />
+            <input
+              type="checkbox"
+              data-testid="trunfo-input"
+              id="cardTrunfo"
+              name="cardTrunfo"
+              value={cardTrunfo}
+              onChange={onInputChange}
+            />
           </label>
-          <button type="submit" data-testid="save-button">
+          <button
+            type="submit"
+            data-testid="save-button"
+            disabled={isSaveButtonDisabled}
+            onClick={onSaveButtonClick}
+          >
             Salvar
           </button>
         </FormStyled>
@@ -76,5 +136,20 @@ function Form() {
     </ContainerForm>
   );
 }
+
+Form.propTypes = {
+  cardName: string.isRequired,
+  cardDescription: string.isRequired,
+  cardAttr1: string.isRequired,
+  cardAttr2: string.isRequired,
+  cardAttr3: string.isRequired,
+  cardImage: string.isRequired,
+  cardRare: string.isRequired,
+  cardTrunfo: bool.isRequired,
+  // hasTrunfo: bool.isRequired,
+  isSaveButtonDisabled: bool.isRequired,
+  onInputChange: func.isRequired,
+  onSaveButtonClick: func.isRequired,
+};
 
 export default Form;
