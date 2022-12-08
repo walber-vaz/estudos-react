0418e3e8bf27cc0193de3480fb9d5fa6055ea7e7
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-08 14:02:19 -0300

    Usando css modules. E como funcionajSX no React funcional

--- react-funcional/src/App.jsx
+++ react-funcional/src/App.jsx
@@ -1,7 +1,18 @@
+import './styles/App.modules.css'
+
 function App() {
+  // Function sum
+  const sum = (a, b) => a + b
+
+  // Image URL placeholder
+  const url = 'https://via.placeholder.com/150'
+
   return (
-    <div>
-      Primeiro projeto React funcional
+    <div className="App">
+      <h1>Walber (w2k)</h1>
+      <p>Olá, mundo!</p>
+      <p>2 + 2 = {sum(2, 2)}</p>
+      <img src={url} alt={url} />
     </div>
   )
 }
