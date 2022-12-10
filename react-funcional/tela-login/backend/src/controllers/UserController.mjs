0edc38fa23a111fc74256aa71673700e5902daad
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-10 00:08:49 -0300

    Criando method PUT para atualizar dados do usuario cadastrado

--- react-funcional/tela-login/backend/src/controllers/UserController.mjs
+++ react-funcional/tela-login/backend/src/controllers/UserController.mjs
@@ -21,14 +21,12 @@ export default {
       return response.status(400).json({ error: 'Missing data' });
     }
 
-    const passwordEncrypted = crypto.createHash('sha256').update(password).digest('hex');
-
     const user = new User({
       _id: uuid(),
       username,
       lastname,
       email,
-      password: passwordEncrypted,
+      password: crypto.createHash('sha256').update(password).digest('hex'),
     });
 
     try {
@@ -36,6 +34,26 @@ export default {
       return response.status(201).json({ message: 'User created' });
     } catch (error) {
       return response.status(400).json({ error: error.message });
+    }
+  },
+
+  async update(request, response) {
+    const { username, lastname, email, password } = request.body;
+
+    if (!username && !lastname && !email && !password) {
+      return response.status(400).json({ error: 'Missing data' });
+    }
+
+    if (username) response.user.username = username;
+    if (lastname) response.user.lastname = lastname;
+    if (email) response.user.email = email;
+    if (password) response.user.password = crypto.createHash('sha256').update(password).digest('hex');
+
+    try {
+      await response.user.save();
+      return response.status(200).json({ message: 'User updated' });
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
     }
   }
 }
--- react-funcional/tela-login/backend/src/routers/routes.mjs
+++ react-funcional/tela-login/backend/src/routers/routes.mjs
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/UserController.mjs';
+import UserMiddlewares from '../middlewares/UserMiddlewares.mjs';
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.get('/', (req, res) => res.json({ message: 'Hello World!' }));
 router.get('/users', UserController.index);
 
 router.post('/users', UserController.create);
+
+router.put('/users/:id', UserMiddlewares.validateId, UserController.update);
 
 export { router };
